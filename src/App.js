86e16553ed84495735a1ef0085963a1976284ea9
rahul237 <rahulbhushanmehta@gmail.com>
2021-09-19 19:17:03 +0530

    x

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
 
+const data = React.useMemo(
+  () => [
+    {
+      col1: 'Hello',
+      col2: 'World',
+    },
+    {
+      col1: 'react-table',
+      col2: 'rocks',
+    },
+    {
+      col1: 'whatever',
+      col2: 'you want',
+    },
+  ],
+  []
+)
+
+const columns = React.useMemo(
+  () => [
+    {
+      Header: 'Column 1',
+      accessor: 'col1', // accessor is the "key" in the data
+    },
+    {
+      Header: 'Column 2',
+      accessor: 'col2',
+    },
+  ],
+  []
+)
+
+const tableInstance = useTable({ columns, data })
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. nnnnnnnnnnnnnnnnnnnnnnnnnnnnnn
         </p>
         <a
           className="App-link"
