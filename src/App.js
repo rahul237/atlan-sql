c949993ffdeba80796c56f18f2799d12a572ea61
rahul237 <rahulbhushanmehta@gmail.com>
2021-09-22 00:18:48 +0530

    remove useless code

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@ import React from 'react';
 
 import './App.css';
 
-
-import { useTable } from 'react-table';
-
 import QueryHome from './screens/query_home';
-import SelectDb from './screens/select_db';
 
 
 function App() {
